efa9c08c8590960ee6d1bee816e8c8aa1f1542db
Gaurav Singh Tomar <116975109+gauravtomar7@users.noreply.github.com>
2023-10-05 23:27:10 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,29 @@
+$("ul").on("click","li",function()
+{
+   $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event)
+{
+  $(this).parent().fadeOut(200,function()
+  {
+   $(this).remove();
+
+  });
+   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event)
+{
+   if(event.which===13)
+   {
+      var to=$(this).val();   
+      $(this).val("");
+      $("ul").append("<li><span><i class='fa-solid fa-trash-can'></i></span> " + to + "</li>");
+   }
+});
+
+$(".fa-plus").click(function()
+{
+   $("input[type='text']").fadeToggle();
+});
